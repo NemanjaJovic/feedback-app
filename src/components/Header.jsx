1346a2c2f4fc8d1cbe3542fa2fd85dde2c5322c4
Nemanja Jovic <nemanja.jovic195@gmail.com>
2022-01-31 00:06:41 +0100

    working with css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,31 @@
 import PropTypes from 'prop-types'
 
-function Header({ text }) {
-  return (<header>
-      <div className="container">
-          <h2>{text}</h2>
-      </div>
-  </header>)
-}
-
-Header.defaultProps = {
-    text: 'Feedback UI',
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
   }
-
-Header.propTypes = {
-    text: PropTypes.string
+  return (
+    <header style={headerStyles}>
+      <div className='container'>
+        <h2>{text}</h2>
+      </div>
+    </header>
+  )
 }
 
-export default Header;
+// Default props
+Header.defaultProps = {
+  text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95',
+}
 
+// Props type checking
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+}
+
+export default Header
