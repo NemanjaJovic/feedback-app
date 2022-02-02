dd323315938263fa3fccf4d717f0aee28bc91c1c
Nemanja Jovic <nemanja.jovic195@gmail.com>
2022-02-02 22:50:59 +0100

    Added rating selection feature

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,28 +1,18 @@
 const FeedbackData = [
   {
     id: 1,
-    rating: 87,
+    rating: 7,
     text: 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.',
   },
   {
     id: 2,
-    rating: 88,
+    rating: 9,
     text: 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.',
   },
   {
     id: 3,
-    rating: 12,
+    rating: 4,
     text: 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
-  },
-  {
-    id: 4,
-    rating: 51,
-    text: 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.',
-  },
-  {
-    id: 5,
-    rating: 48,
-    text: 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
   },
 ]
 
