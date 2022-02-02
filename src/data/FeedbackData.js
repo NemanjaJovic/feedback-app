f3d22b002c30e840bea8c96b7ad2b8e173f5d926
Nemanja Jovic <nemanja.jovic195@gmail.com>
2022-02-02 23:04:41 +0100

    Added uuid lib for unique IDs and add feedback feature

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,16 +1,16 @@
 const FeedbackData = [
   {
-    id: 1,
+    id: 'edfjdslvckoikhjb4525',
     rating: 7,
     text: 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.',
   },
   {
-    id: 2,
+    id: 'erefdfsdjfklj4234',
     rating: 9,
     text: 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.',
   },
   {
-    id: 3,
+    id: 'errr34aasdccv4',
     rating: 4,
     text: 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
   },
